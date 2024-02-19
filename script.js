6c1fb99e872d067486f488a933e4efa644c79e76
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-19 11:48:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const charFrequency = {};
+
+  for (let char of str) {
+    if (charFrequency[char]) {
+      charFrequency[char]++;
+    } else {
+      charFrequency[char] = 1;
+    }
+  }
+
+  for (let char of str) {
+    if (charFrequency[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
